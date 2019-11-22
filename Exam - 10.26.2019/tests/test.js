65ec616fa4997ec3df79eca76cab9a73710e202e
Martin Asenov <m.asenov5752@gmail.com>
2019-11-22 10:14:41 +0200

    removed exam

--- Exam - 10.26.2019/tests/test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-
-describe('test', function() {
-    describe('first group', function() {
-        if ('test', function() {
-                expect(0 === 0).to.be.true;
-            });
-    });
-});
